a16f13ed3e454d2bba8371971a247a372d510139
MilagrosRn <milinr29@gmail.com>
2024-11-17 15:57:24 -0500

    fix: resolve observations html

---
+++ prototipo-esperado-entrega/src/app.js
@@ -0,0 +1,45 @@
+let productsShow = [];
+
+const getAllProducts = () => {
+  return new Promise((resolve, reject) => {
+    fetch('https://dummyjson.com/products')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error en la respuesta de la API');
+        }
+        return response.json();
+      })
+      .then(res => {
+        productsShow = res.products;  // Guardamos los productos en la variable
+        resolve(productsShow);  // Resolvemos la promesa con los productos
+      })
+      .catch(error => {
+        reject(error);  // Rechazamos la promesa si ocurre un error
+      });
+  });
+};
+
+const searchProduct= async ()=>{
+    try{ 
+    const resp=  await fetch('https://dummyjson.com/products/search?q=phone')
+    if (!resp.ok) {
+        throw new Error('Error en la solicitud');
+      }
+    const data= await resp.json()
+    console.log(data)
+    }
+
+    catch(error){
+        console.error('Hubo un error al buscar el producto:', error);
+    }
+}
+// uso de la función then y catch tambien
+// getAllProducts()
+//   .then(products => {
+//     console.log('Productos cargados:', products);
+//   })
+//   .catch(error => {
+//     console.error('Hubo un error:', error);
+//   });
+
+searchProduct()
